f8f10027b0bcbd2131b06696df0b7c79fd6c8e5b
sahrfearmacarthy <macarthysahralves>
2024-09-18 08:15:39 -0700

    Fix bugs

--- Server/app.ts
+++ Server/app.ts
@@ -1,3 +1,38 @@
+// import express, { NextFunction, Request, Response } from "express";
+// import { messaage_route } from "./router";
+// import { ErrorWithStatus } from "./error";
+// import cors from "cors";
+// import "dotenv/config";
+
+// //init
+// const app = express();
+
+// app.set("port", 3000);
+
+// //midware
+// app.use(cors());
+
+// //routes
+// app.use("", messaage_route);
+
+// //error handler
+// app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+//   if (err instanceof ErrorWithStatus) {
+//     res.status(err.status).send(err.message);
+//   } else if (err instanceof Error) {
+//     res.status(5000).send(err.message);
+//   } else {
+//     res.status(500).send("Unknown Error");
+//   }
+// });
+
+// //bootstarp
+// app.listen(app.get("port"), () => {
+//   console.log("Listining to port " + " " + app.get("port"));
+// });
+// module.exports = app;
+
+// export default app;
 import express, { NextFunction, Request, Response } from "express";
 import { messaage_route } from "./router";
 import { ErrorWithStatus } from "./error";
@@ -7,29 +42,30 @@ import "dotenv/config";
 //init
 const app = express();
 
-app.set("port", 3000);
+// Use process.env.PORT for Vercel deployment
+const port = process.env.PORT || 3000;
+app.set("port", port);
 
-//midware
+// Middleware
 app.use(cors());
 
-//routes
+// Routes
 app.use("", messaage_route);
 
-//error handler
+// Error handler
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ErrorWithStatus) {
     res.status(err.status).send(err.message);
   } else if (err instanceof Error) {
-    res.status(5000).send(err.message);
+    res.status(500).send(err.message);
   } else {
     res.status(500).send("Unknown Error");
   }
 });
 
-//bootstarp
+// Bootstrapping server
 app.listen(app.get("port"), () => {
-  console.log("Listining to port " + " " + app.get("port"));
+  console.log(`Listening on port ${app.get("port")}`);
 });
-module.exports = app;
 
 export default app;
